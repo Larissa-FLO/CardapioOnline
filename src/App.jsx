1e0f8c3515f768d12b997b8e3db54b4016cbba6b
Larissa Lopes de Oliveira <larissa.arqueo@outlook.com>
2024-02-01 14:16:58 -0300

    resolvendo problemas com as imagens

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
     <>
       <div className='header'>
 
-        <span><img src='./src/img/LogoQuentinhas.png'></img></span>
+        <span><img src="https://document-export.canva.com/T_2nk/DAF7AoT_2nk/10/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20240201%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240201T053135Z&X-Amz-Expires=45585&X-Amz-Signature=293c97c899252ce3ddcb8469a4278323cfb83c52f0ebbc39675905fb60ffaecc&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2001%20Feb%202024%2018%3A11%3A20%20GMT"></img></span>
 
         <div className='menu'>
-          <button><img src='./src/img/almoco.svg' className='icones'/><span>Almoço</span></button>
-          <button><img src='./src/img/jantar.svg' className='icones'/><span>Jantar</span></button>
-          <button><img src='./src/img/sobremesa.svg' className='icones'/><span>Sobremesas</span></button>
-          <button><img src='./src/img/bebidas.svg' className='icones'/><span>Bebidas</span></button>
+          <button><img src='https://document-export.canva.com/URK0I/DAF7gaURK0I/5/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20240201%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240201T004920Z&X-Amz-Expires=65437&X-Amz-Signature=c77cd4861573da1dd2a9c7defef1b1b56563d6a0ab5c4cefa6149cdfed5a1ae4&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2001%20Feb%202024%2018%3A59%3A57%20GMT' className='icones'/><span>Almoço</span></button>
+          <button><img src='https://document-export.canva.com/EPrPs/DAF7gREPrPs/5/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20240131%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240131T212345Z&X-Amz-Expires=76563&X-Amz-Signature=f18dcfc27142fbdf4c75e5ae8e8981e8b24a2fa56dddbb1b63704475cd358949&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2001%20Feb%202024%2018%3A39%3A48%20GMT' className='icones'/><span>Jantar</span></button>
+          <button><img src='https://document-export.canva.com/6vg8E/DAF7gB6vg8E/21/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20240201%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240201T101223Z&X-Amz-Expires=30099&X-Amz-Signature=5668836aa8ead68bced86c0baaca8f1dfd76bfb6ffc7552786f8aecf3738d4cc&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2001%20Feb%202024%2018%3A34%3A02%20GMT' className='icones'/><span>Sobremesas</span></button>
+          <button><img src='https://document-export.canva.com/7K8og/DAF7gS7K8og/5/thumbnail/0001.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAQYCGKMUHWDTJW6UD%2F20240201%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240201T063342Z&X-Amz-Expires=43970&X-Amz-Signature=c20912edb3c3a9ce267ada4654ba79b97f64b17d74cfb1333de331e57bf52071&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2001%20Feb%202024%2018%3A46%3A32%20GMT' className='icones'/><span>Bebidas</span></button>
         </div>
 
         <div className='contato'>
